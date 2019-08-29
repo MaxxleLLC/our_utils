3bd8117188feb6e5aef5a0a5a3fa87d3341e98bb
IlyaFaer <ilya.faer@mail.ru>
2019-08-29 18:49:35 +0300

    Script for docs returning nextgen

--- script.py
+++ script.py
@@ -1,69 +1,87 @@
 import inspect
 import re
-import bigquery.google.cloud.bigquery
+import google.cloud.bigquery
+import google.cloud.bigquery.dbapi
 
+# :type[\s]+(?P<arg_name>[\S]+):
+# [\s\n]+(?P<arg_type>[\S\s]+)
+# (:raises:[\s]+(?P<raises>[\s\S\n]+))?
+# :param[\s]+[\S]+:[\s\n]+(?P<arg_doc>[\S\s]+)
 
-OLD_TYPES = re.compile(
+SPACES = {"method": "        ", "func": "    "}
+OLD_RETURN = re.compile(
     r"""
-    :type[\s]+(?P<arg_name>[\S]+):[\s\n]+(?P<arg_type>[\S\s]+)\n
-    :param[\s]+[\S]+:[\s\n]+(?P<arg_doc>[^:]+)
-    # :rtype:(?P<rtype>[\s\S\n]+)\n
-    # :returns:(?P<returns>[\s\S\n]+)(\n|$)
-    # (:raises:[\s]+(?P<raises>[\s\S\n]+))?
+    :rtype:[\s]+(?P<rtype>[\s\S\n]+)\n
+    :returns:[\s]?(?P<returns>[^.$]+)($|.$)
     """,
-    re.VERBOSE
+    re.VERBOSE,
 )
 
-def check_old_types(docs):
-    if ':type' in docs:
-        print('docs: ')
-        print(docs)
-        print('++++++++')
-        match = OLD_TYPES.search(docs)
-        if match is not None:
-            print('regex_name: ', match.group("arg_name"))
-            print()
-            print('regex_type: ', match.group("arg_type"))
-            print()
-            print('regex_doc: ')
-            print(match.group("arg_doc"))
-            # print()
-            # print('regex_raises: ')
-            # print(match.group("raises"))
-            # print()
-            # print('rtype: ')
-            # print(match.group("rtype"))
-            # print()
-            # print('returns: ')
-            # print(match.group("returns"))
+processed_objects = []
 
-        print('#############')
+
+def write_to_file(old, new, addr):
+    if old and new:
+        file = open(addr, "r")
+        lines = file.read()
+        file.close()
+        lines = lines.replace(old, new)
+        file = open(addr, "w")
+        file.write(lines)
+        file.close()
+
+
+def add_spaces(old_string, type_):
+    old_string = old_string.split("\n")
+    new_string = "\n".join([SPACES[type_] + line for line in old_string])
+    return new_string
+
+
+def replacements_for_args(docs, type_):
+    rold = rnew = None
+    if ":rtype" in docs:
+        match = OLD_RETURN.search(docs)
+        if match is not None:
+            rold = match.group(0)
+
+            rnew = """Returns:
+    {rtype}: {return_doc}.""".format(
+                rtype=match.group("rtype"), return_doc=match.group("returns")
+            )
+            rold = add_spaces(rold, type_)
+            rnew = add_spaces(rnew, type_)
+
+    return rold, rnew
 
 
 def process_members(obj):
     for name, member in inspect.getmembers(obj):
-        if name.startswith('__'):
+        if member in processed_objects or name.startswith("__"):
             continue
 
-        if inspect.isfunction(member):
-            docs = inspect.getdoc(member)
-            # print('func: ', name)
-            # print('docs: ')
-            # print(docs)
-        elif inspect.ismethod(member):
-            docs = inspect.getdoc(member)
-            print('method: ', name)
-            if docs:
-                check_old_types(docs)
-        elif inspect.isclass(member):
-            docs = inspect.getdoc(member)
-            print('class: ', name)
-            # print('docs: ')
-            # print(docs)
+        try:
+            addr = inspect.getsourcefile(obj)
+        except TypeError:
+            continue
 
+        if "bigquery" not in addr:
+            continue
+
+        is_method = inspect.ismethod(member) or isinstance(member, property)
+        is_func = inspect.isfunction(member)
+
+        if is_method or is_func:
+            docs = inspect.getdoc(member)
+            if docs:
+                type_ = "method" if is_method else "func"
+                old, new = replacements_for_args(docs, type_)
+                write_to_file(old, new, addr)
+
+        elif inspect.isclass(member) or inspect.ismodule(member):
             process_members(member)
 
-        print('--------------')
+        processed_objects.append(obj)
 
 
-process_members(bigquery.google.cloud.bigquery)
+process_members(google.cloud.bigquery)
+process_members(google.cloud.bigquery.dbapi)

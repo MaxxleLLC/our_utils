e0a2c306ada0ac7f2622c15425810602f8cd4f3f
IlyaFaer <ilya.faer@mail.ru>
2019-09-01 11:20:51 +0300

    Args title and params shift

--- script2.py
+++ script2.py
@@ -8,6 +8,8 @@ OLD_TYPE = re.compile(r":type[\s]+(?P<arg_name>[\S]+):[\s\n]+(?P<arg_type>[\S\s]
 OLD_PARAM = re.compile(r":param[\s]+([\S]+):(?P<arg_docs>[\S\s]+)")
 SPACES = re.compile(r"(?P<spaces>[\s]+):param")
 
+NUM_SPACES = {"func": " " * 4, "method": " " * 8}
+
 processed_objects = []
 
 
@@ -19,6 +21,16 @@ def write_to_file(old, new, addr):
         lines = lines.replace(old, new)
         with open(addr, "w") as file:
             file.write(lines)
+
+
+def add_spaces(docs, spaces):
+    docs = docs.split("\n")
+    for index, line in enumerate(docs):
+        if line:
+            docs[index] = spaces + " " * 4 + line.lstrip()
+
+    docs = "\n" + "\n".join(docs)
+    return docs
 
 
 def get_indexes(string, start, end, temp):
@@ -39,38 +51,29 @@ def strip_n_news(string):
     return string
 
 
-def format_docs(docs, tag, num):
+def format_docs(docs, tag, num, type_):
     if tag == ":type ":
         match_type, type_statement = get_indexes(docs, tag, ":param ", OLD_TYPE)
         match_param, param_statement = get_indexes(docs, ":param ", "\n\n", OLD_PARAM)
-
-        if "Check client or verify over-ride." in docs:
-            print()
 
         if match_param is not None and match_type is not None:
             arg_docs = match_param.group("arg_docs")
             if "\n" in arg_docs:
                 if not arg_docs.startswith("\n"):
                     spaces = SPACES.search(docs).group("spaces")[1:] + " " * 4
-                    arg_docs = arg_docs.split("\n")
-                    for index, line in enumerate(arg_docs):
-                        if line:
-                            arg_docs[index] = spaces + line.lstrip()
-                    arg_docs = "\n" + "\n".join(arg_docs)
                 else:
                     arg_docs = arg_docs[1:]
                     spaces = " " * (len(arg_docs) - len(arg_docs.lstrip()))
-                    arg_docs = arg_docs.split("\n")
-                    for index, line in enumerate(arg_docs):
-                        if line:
-                            arg_docs[index] = spaces + line.lstrip()
-                    arg_docs = "\n" + "\n".join(arg_docs)
+
+                arg_docs = add_spaces(arg_docs, spaces)
             else:
                 arg_docs = " " + arg_docs.lstrip()
 
             parts = docs.split(type_statement + "\n")
             docs = parts[0] + parts[1].lstrip()
-            new_param = "{name} ({type_}):{docs}".format(
+
+            new_param = "{title}    {name} ({type_}):{docs}".format(
+                title="Args:\n" + NUM_SPACES[type_] if num == 0 else "",
                 name=match_type.group("arg_name"),
                 type_=match_type.group("arg_type"),
                 docs=arg_docs,
@@ -85,12 +88,19 @@ def replacements_for_args(docs, type_):
     rold = docs
     for tag in (":type ",):
         for num in range(docs.count(tag)):
-            docs = format_docs(docs, tag, num)
+            docs = format_docs(docs, tag, num, type_)
     return rold, docs
 
 
 def untouched(docs):
     return docs
+
+
+def rewrite_docs(docs, type_, addr):
+    if docs:
+        old, new = replacements_for_args(docs, type_)
+        if new and old:
+            write_to_file(old, new, addr)
 
 
 def process_members(obj):
@@ -109,24 +119,18 @@ def process_members(obj):
 
             is_method = inspect.ismethod(member) or isinstance(member, property)
             is_func = inspect.isfunction(member)
+            docs = inspect.getdoc(member)
 
             if is_method or is_func:
                 if not is_method:
                     is_method = "self" in inspect.getfullargspec(member).args
-                docs = inspect.getdoc(member)
-                if docs:
-                    type_ = "method" if is_method else "func"
-                    old, new = replacements_for_args(docs, type_)
-                    if new and old:
-                        write_to_file(old, new, addr)
+
+                type_ = "method" if is_method else "func"
+                rewrite_docs(docs, type_, addr)
 
             elif inspect.isclass(member) or inspect.ismodule(member):
                 if inspect.isclass(member):
-                    docs = inspect.getdoc(member)
-                    if docs:
-                        old, new = replacements_for_args(docs, "func")
-                        if new and old:
-                            write_to_file(old, new, addr)
+                    rewrite_docs(docs, "func", addr)
 
                 process_members(member)
 
